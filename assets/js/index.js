cb14c6cdca9d52c562fa5f9b60c5d65b4fbaf9ad
Nick Iantosca <niantosca805@gmail.com>
2018-10-29 16:14:37 -0400

    Updated Functionality/Reset function almost working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,118 +1,90 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var random = Math.floor(Math.random() * 102 + 19);
+    $("#number").html("Number: " + random);
+    var gem1 = Math.floor(Math.random() * 12 + 1);
+    var gem2 = Math.floor(Math.random() * 12 + 1);
+    var gem3 = Math.floor(Math.random() * 12 + 1);
+    var gem4 = Math.floor(Math.random() * 12 + 1);
+    var compTotal = random;
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
 
-    var random = Math.floor(Math.random()*102+19);
-    
-    $("#number").text(random);
-
-
-
-var gem1= Math.floor(Math.random()*12+1);
-var gem2= Math.floor(Math.random()*12+1);
-var gem3= Math.floor(Math.random()*12+1);
-var gem4= Math.floor(Math.random()*12+1);
-var compTotal= random;
-var userTotal= 0;
-var wins = 0;
-var losses = 0;
-
-
-
-
-
-$("#image1").click(function() {
-
-    userTotal = userTotal + gem1;
-   
-    
-    if (userTotal === compTotal) {
-        wins++;
-    }
-   
-    else if (userTotal > compTotal) {
-        losses++;
-
-    }
-    
-    $("#user").html(userTotal);
+    function reset() {
+        var random = Math.floor(Math.random() * 102 + 19);
+        $("#number").html("Number: " + random);
+        gem1 = Math.floor(Math.random() * 12 + 1);
+        gem2 = Math.floor(Math.random() * 12 + 1);
+        gem3 = Math.floor(Math.random() * 12 + 1);
+        gem4 = Math.floor(Math.random() * 12 + 1);
+        var userTotal = 0;
+        $("#user").text("Your Score: " + userTotal);
+        }; 
+  //win alert function and adding wins to the tally
+  function winning() {
+  alert("You won!");
+    wins++; 
     $("#bing").html("Wins: " + wins);
+    reset();
+  }
+  //loss alert function and adding losses to the tally
+  function losing() {
+  alert ("You lose!");
+    losses++;
     $("#bong").html("Losses: " + losses);
+    reset();
+  }
 
 
+    $("#image1").click(function () {
+        userTotal = userTotal + gem1;
+        console.log(userTotal);
+        if (userTotal === compTotal) {
+            winning();
+        } else if (userTotal > compTotal) {
+            losing();
+        }
+        $("#user").html("Your Score: " + userTotal);
+    });
 
-});
+    $("#image2").click(function () {
+        userTotal = userTotal + gem2;
+        console.log(userTotal);
+        if (userTotal === compTotal) {
+            winning();
+        } else if (userTotal > compTotal) {
+            losing();
+        }
+        $("#user").html("Your Score: " + userTotal);
+    });
 
-$("#image2").click(function() {
+    $("#image3").click(function () {
+        userTotal === userTotal + gem3;
+        console.log(userTotal);
+        if (userTotal === compTotal) {
+            winning();
+        } else if (userTotal > compTotal) {
+            losing();
+        }
+        $("#user").html("Your Score: " + userTotal);
+    });
 
-    userTotal = userTotal + gem2;
-    console.log(userTotal);
+    $("#image4").click(function () {
+        userTotal === userTotal + gem4;
+        console.log(userTotal);
+        if (userTotal === compTotal) {
+            winning();
+        } else if (userTotal > compTotal) {
+            losing();
+        }
+        $("#user").html("Your Score: " + userTotal);
+    });
 
-    if (userTotal === compTotal) {
-        wins++;
-    }
-
-    else if (userTotal > compTotal) {
-        losses++;
-
-    }
-
-    $("#user").html(userTotal);
-    $("#bing").html("Wins: " + wins);
-    $("#bong").html("Losses: " + losses);
-
-
-
-});
-
-$("#image3").click(function() {
-
-    userTotal === userTotal + gem2;
-    console.log(userTotal);
-
-    if (userTotal === compTotal) {
-        wins++;
-    }
-
-    else if (userTotal > compTotal) {
-        losses;
-
-    }
-
-    $("#user").html(userTotal);
-    $("#bing").html("Wins: " + wins);
-    $("#bong").html("Losses: " + losses);
-
-
-
-});
-
-$("#image4").click(function() {
-
-    userTotal === userTotal + gem2;
-    console.log(userTotal);
-
-    if (userTotal === compTotal) {
-        wins++;
-    }
-
-    else if (userTotal > compTotal) {
-        losses++;
-
-    }
-
-    $("#user").html(userTotal);
-    $("#bing").html("Wins: " + wins);
-    $("#bong").html("Losses: " + losses);
-
-
-
-});
-
-//$("#bing").html= "Wins: " + wins;
-//$("#bong").html= "Losses: " + losses;
+    //$("#bing").html= "Wins: " + wins;
+    //$("#bong").html= "Losses: " + losses;
 
 
 
 
 
 });
-        
